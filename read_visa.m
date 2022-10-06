04721c40de85fc045c096bfc6b9b077a258f090a
tsopeter <70542629+tsopeter@users.noreply.github.com>
2022-10-06 14:44:38 -0500

    Got rid of TYPE
    The TYPE COMMAND is currently not implemented yet so it is redundant. All data returned are [doubles].

--- read_visa.m
+++ read_visa.m
@@ -1,6 +1,7 @@
-function [data, space] = read_visa(GPIB_IDN, CMD, TYPE)
+function [data, space] = read_visa(GPIB_IDN, CMD)%, TYPE)
 % If you want to get data from the visa device
 % use CMD = "--RDATA" for read_data
+% although the command asks for type, the TYPE COMMAND is not used
 
 span = ask_visa(GPIB_IDN, 'SPANWL?;');
 center = ask_visa(GPIB_IDN, 'CENTERWL?;');
@@ -11,7 +12,7 @@ center = ask_visa(GPIB_IDN, 'CENTERWL?;');
 lb = center - (span/2);
 ub = center + (span/2);
 
-data = rvisa_implt(GPIB_IDN, CMD, TYPE);
+data = rvisa_implt(GPIB_IDN, CMD, 'NOT_USED');
 
 len   = length(data);
 space = linspace(lb, ub, len);
